51e578e4f41e5ef3813be32e5bd09eb5674b51d2
Kano Ichikawa <ec25678@qmul.ac.uk>
2026-09-15 14:05:17 +0100

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i=0; i<nums.length-1; i++){
+            for (int n=i+1; n<nums.length; n++){
+                if(nums[i]==nums[n]){return true;}
+
+            }
+        }
+        return false;
+    }
+}
